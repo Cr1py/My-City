ad7f2174874b502e4fe8f5c34bacab9cb50752ba
OtisDing <93888172+OtisDing@users.noreply.github.com>
2023-03-31 11:36:04 -0400

    Add files via upload

---
+++ lib/preview.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'data.dart';
+import 'small_target.dart';
+
+class Preview extends StatefulWidget {
+  final int listName;
+
+  const Preview({Key? key, required this.listName}) : super(key: key);
+
+  @override
+  State<Preview> createState() => _PreviewState();
+}
+
+class _PreviewState extends State<Preview> {
+  List<smallTarget> getTargets(int count, int increment){
+    List<smallTarget> list = [];
+
+    for (int i = count; i < count + increment; i++){
+      String image = imageList[widget.listName][i];
+      var newItem = smallTarget(path: "$image");
+
+      list.add(newItem);
+    }
+
+    return list;
+  }
+
+  List<Widget> getRows(){
+    List<Row> rows = [];
+    int count = 0;
+    int increment = 8;
+    int numRows = 8;
+
+    for (int i = 0; i < numRows; i++) {
+      List targets = getTargets(count, increment);
+      var newRow = Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ...targets,
+        ],
+      );
+      rows.add(newRow);
+      count = count + increment;
+    }
+    return rows;
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200.0,
+      height: 200.0,
+      child: Column(
+        children: [
+          ...getRows(),
+        ],
+      ),
+    );
+  }
+}
+
+
